1d1f6fffb1332fd9485664fd7b4ceabe6c4ce295
Wim den Braven <wimdenbraven@persuavis.com>
2024-01-08 15:08:23 -0800

    Initial commit

---
+++ lib/bp3/activestorage.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'activestorage/version'
+
+module Bp3
+  module Activestorage
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/bp3/activestorage_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bp3::Activestorage do
+  it 'has a version number' do
+    expect(Bp3::Activestorage::VERSION).not_to be_nil
+  end
+
+  it 'does something useful' do
+    expect(false).to be(true)
+  end
+end
