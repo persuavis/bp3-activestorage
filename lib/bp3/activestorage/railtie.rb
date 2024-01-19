3e9cba6f75388a8783b8ee1ecb0ec07b9834670c
Wim den Braven <wimdenbraven@persuavis.com>
2024-01-19 15:36:53 -0800

    Update controller adaptations

--- lib/bp3/activestorage/railtie.rb
+++ lib/bp3/activestorage/railtie.rb
@@ -12,6 +12,7 @@ module Bp3
             ::ActiveStorage::Blob # preload
             ::ActiveStorage::VariantRecord # preload
             ::ActiveStorage::BaseController # preload
+            ::ActiveStorage::Representations::ProxyController # preload
             module ::ActiveStorage
               # CAUTION: do not include Tenantable, it breaks things (it may set site/tenant behind the scenes,
               #   and sets the default scope, and then later the blob can't be found
@@ -64,7 +65,25 @@ module Bp3
 
               class BaseController
                 include Bp3::Core::Actions
+                include Bp3::Core::Settings
+                include Bp3::Core::FeatureFlags
                 include Bp3::Core::Cookies
+
+                # TODO: is this needed?
+                # before_action :authenticate_root!
+              end
+
+              module Representations
+                class ProxyController
+                  include Bp3::Core::Actions
+                  include Bp3::Core::Settings
+                  include Bp3::Core::FeatureFlags
+                  include Bp3::Core::Cookies
+
+                  # using authenticate_root! breaks things
+                  # TODO: is this needed?
+                  # before_action :authenticate_root!
+                end
               end
             end
           end
